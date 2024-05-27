17be8802715eb17a3dc9c787b7d57452bd6d47f6
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-05-27 14:36:25 +0700

    menambahkan pesan joko public seseorang

--- Polymorphism.cpp
+++ Polymorphism.cpp
@@ -9,13 +9,13 @@ public:
     //}
 };
 
-
 class joko :public seseorang {
 public:
     void pesan() {
         cout << "pesan dari joko" << endl;
     }
 };
+
 
 class lia :public seseorang {
 public:
