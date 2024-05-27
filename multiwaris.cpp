6787e46bd4992949ec6c728e7eb1782bbff59e67
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-05-27 15:01:11 +0700

    membuat class orang dengan mengisi data umur

---
+++ multiwaris.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+    int umur;
+
+    orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur" << umur << "\n" << endl;
+    }
+};
+
+
+class pekerja : virtual public orang 
